11a76fa3fcf51984166a84eb904743b78ad56241
bengkelgadget-code <bengkelgadget.l2@gmail.com>
2026-05-01 09:44:14 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 // Vercel API Proxy - Bypass CORS untuk Google Apps Script
 // Letakkan file ini di folder: /api/proxy.js
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzFDsa85R56Nlgn3QwZ_hl3SNVybUtBY0GjgP1LAF-jN_YfTSyA9Dz2XEK6YB7qDNFqlw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbw4LsV2mB_x517QfNxQtA4AQmdYzyaUNPp0KCcC1F-_o-0wJtUaKYvdlqKmZcWBKq4Cyw/exec";
 
 export default async function handler(req, res) {
     // Allow CORS dari Vercel app
